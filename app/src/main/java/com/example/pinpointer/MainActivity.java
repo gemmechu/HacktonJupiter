5636c19f38885c541e265f7bc0cef0a3cdd5d59b
zekaryas <zekaryasdinku@gmail.com>
2019-05-11 09:03:06 +1000

    navigation drawer implementation

--- app/src/main/java/com/example/pinpointer/MainActivity.java
+++ app/src/main/java/com/example/pinpointer/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.pinpointer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar =  findViewById(R.id.toolbarindrawer);
+        toolbar.setTitle(R.string.app_name);
+
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        String itemName = (String)item.getTitle();
+
+        Toast.makeText(this,itemName+" clicked",Toast.LENGTH_SHORT).show();
+        closeDrawer();
+        return true;
+    }
+    private void closeDrawer(){
+        drawerLayout.closeDrawer(GravityCompat.START);
     }
 }
