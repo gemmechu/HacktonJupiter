42de84c974e8726a19782c89d7115dd6a82e13b8
Gemmechu <gemmechumohammed@gmail.com>
2019-05-11 14:18:19 +0300

    navigation to fragment

--- app/src/main/java/com/example/pinpointer/Activity/MainActivity.java
+++ app/src/main/java/com/example/pinpointer/Activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.pinpointer;
+package com.example.pinpointer.Activity;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,13 +12,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.pinpointer.Fragment.AddLocationFragment;
+import com.example.pinpointer.Fragment.VerifyLocationFragment;
+import com.example.pinpointer.R;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
     private NavigationView navigationView;
-
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +42,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        fragment = null;
+        switch (item.getItemId()) {
 
-        String itemName = (String)item.getTitle();
+            case R.id.addLocation:
+                Toast.makeText(this," location clicked",Toast.LENGTH_SHORT).show();
+                fragment = new AddLocationFragment();
+                // Not implemented here
+                break;
+            case R.id.verifyLocation:
+                fragment = new VerifyLocationFragment();
+                Toast.makeText(this," verify clicked",Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(this,itemName+" clicked",Toast.LENGTH_SHORT).show();
+                // Not implemented here
+            break;
+            default:
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
         closeDrawer();
         return true;
     }
---
+++ app/src/main/java/com/example/pinpointer/model/sample.java
@@ -0,0 +1,22 @@
+package com.example.pinpointer.model;
+
+public class sample {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String name;
+    public String description;
+}
