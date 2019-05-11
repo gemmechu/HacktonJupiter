16b21697cba986d43548b785bc8e9469604250f4
tensaeb <tensaeb2017@gmail.com>
2019-05-11 19:08:32 +0300

    Add location UI

--- app/src/main/java/com/example/pinpointer/Activity/AddLocation.java
+++ app/src/main/java/com/example/pinpointer/Activity/AddLocation.java
@@ -2,13 +2,27 @@ package com.example.pinpointer.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.example.pinpointer.R;
 
 public class AddLocation extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.institution, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 }
