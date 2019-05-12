9c579d035b63b2871b471bb4625c537bb1071965
zekaryas <zekaryasdinku@gmail.com>
2019-05-12 18:42:58 +1000

    Merge branch 'master' of https://github.com/gemmechu/HacktonJupiter
    # Conflicts:
    #	app/src/main/java/com/example/pinpointer/Activity/MainActivity.java

--- app/src/main/java/com/example/pinpointer/Activity/MainActivity.java
+++ app/src/main/java/com/example/pinpointer/Activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.pinpointer.Activity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -24,26 +28,30 @@ import android.widget.Toast;
 import com.example.pinpointer.Api.Location;
 import com.example.pinpointer.Api.LocationPlaceHolder;
 import com.example.pinpointer.Fragment.HomeFragment;
+import com.example.pinpointer.Fragment.SearchFragment;
 import com.example.pinpointer.Fragment.VerifyLocationFragment;
+import com.example.pinpointer.LocaleHelper;
+import com.example.pinpointer.Main;
 import com.example.pinpointer.R;
 
 import java.util.List;
 
+import io.paperdb.Paper;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
     private Toolbar toolbar;
     private NavigationView navigationView;
     Fragment fragment;
-
-    public static final String MyPREFERENCES = "MyPrefs" ;
+    TextView heyTv,below;
+    public static final String MyPREFERENCES = "MyPrefs";
     public static final String longitude = "longitude";
     public static final String latitude = "latitude";
     SharedPreferences sharedpreferences;
@@ -59,29 +67,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
 
-        toolbar =  findViewById(R.id.toolbarindrawer);
+        toolbar = findViewById(R.id.toolbarindrawer);
 
         navigationView = (NavigationView) findViewById(R.id.navigationView);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        final Switch langSwitch=findViewById(R.id.switch_lang);
+        final Switch langSwitch = findViewById(R.id.switch_lang);
         navigationView.setNavigationItemSelectedListener(this);
         fragment = new HomeFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-
-
+        heyTv=findViewById(R.id.hey_tv);
+        below=findViewById(R.id.below);
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         editor = sharedpreferences.edit();
-
+        Paper.init(this);
         search = findViewById(R.id.search);
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                chekNetwork();
                 EditText searchKey = findViewById(R.id.searchKey);
-                Toast.makeText(MainActivity.this,searchKey.getText().toString(),Toast.LENGTH_LONG).show();
+                Bundle bundle = new Bundle();
+                bundle.putString("search", searchKey.getText().toString());
+
+
+                fragment = new SearchFragment();
+                fragment.setArguments(bundle);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                Toast.makeText(MainActivity.this, searchKey.getText().toString(), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -90,8 +106,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     }
+    @SuppressLint("ResourceAsColor")
+    public void chekNetwork(){
+        if(!isNetworkAvailable()){
 
-    public void getLocation(){
+            Toast toast= Toast.makeText(getApplicationContext(),"Ops!! check Your Connection",Toast.LENGTH_LONG);
+            View view =toast.getView();
+            view.setBackgroundResource(R.color.amber_500);
+
+            TextView textView=view.findViewById(android.R.id.message);
+
+            textView.setTextColor(R.color.white);
+            toast.show();
+        }
+
+    }
+
+    public void getLocation() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.ipfind.com/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -105,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         call.enqueue(new Callback<Location>() {
             @Override
             public void onResponse(Call<Location> call, Response<Location> response) {
-                if(!response.isSuccessful()){
+                if (!response.isSuccessful()) {
                     return;
                 }
 
@@ -114,21 +145,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 editor.putString(longitude, location.getLongitude());
                 editor.commit();
                 TextView tv = findViewById(R.id.longitude);
-                tv.setText("Longitude "+ sharedpreferences.getString(longitude,"Unable to get longitude"));
+                tv.setText("Longitude " + sharedpreferences.getString(longitude, "Unable to get longitude"));
                 TextView tv2 = findViewById(R.id.latitude);
-                tv2.setText("Latitude "+ sharedpreferences.getString(latitude,"Unable to get latitude"));
+                tv2.setText("Latitude " + sharedpreferences.getString(latitude, "Unable to get latitude"));
 
 
             }
 
             @Override
             public void onFailure(Call<Location> call, Throwable t) {
-                Toast.makeText(MainActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
 
     }
-
 
 
     @Override
@@ -142,16 +172,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             case R.id.language:
                 fragment = new HomeFragment();
+                if(Main.langCheck==0){
+
+                    Paper.book().write("language","am");
+                    updateView((String) Paper.book().read("language"));
+                    Main.langCheck=1;
+                }
+                else{
+
+                    Paper.book().write("language","en");
+                    updateView((String) Paper.book().read("language"));
+                    Main.langCheck=0;
+                }
                 break;
             case R.id.addLocation:
-                Intent i = new Intent(MainActivity.this,AddLocation.class);
+                Intent i = new Intent(MainActivity.this, AddLocation.class);
                 startActivity(i);
                 return true;
             case R.id.verifyLocation:
                 fragment = new VerifyLocationFragment();
 
                 break;
-                // Not implemented here
+            // Not implemented here
 
             default:
                 break;
@@ -161,7 +203,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         closeDrawer();
         return true;
     }
-    private void closeDrawer(){
+
+    private void closeDrawer() {
         drawerLayout.closeDrawer(GravityCompat.START);
     }
-}
+
+    public void updateView(String lang) {
+        Context context = LocaleHelper.setLocale(getApplicationContext(), lang);
+        Resources resources = context.getResources();
+        heyTv.setText(resources.getString(R.string.hey_there));
+        below.setText(resources.getString(R.string.find_nearby_places));
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }}
--- app/src/main/java/com/example/pinpointer/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/pinpointer/Fragment/SearchFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pinpointer.R;
 
@@ -13,7 +15,7 @@ import com.example.pinpointer.R;
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
-
+TextView tv;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -24,7 +26,16 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view=inflater.inflate(R.layout.fragment_search, container, false);
+
+        String strtext = getArguments().getString("search");
+        search(strtext);
+        return view;
     }
+
+    private void search(String strtext) {
+        Toast.makeText(getContext(),strtext,Toast.LENGTH_LONG).show();
+    }
+
 
 }
